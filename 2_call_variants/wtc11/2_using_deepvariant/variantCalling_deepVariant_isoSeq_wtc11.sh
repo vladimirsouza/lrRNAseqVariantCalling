e0df5aad95c5291f21311227bbde669346204193
vladimirsouza <vladimirbcs@gmail.com>
2022-01-20 12:39:08 +0100

    Update variantCalling_deepVariant_isoSeq_wtc11.sh

--- 2_call_variants/wtc11/2_using_deepvariant/variantCalling_deepVariant_isoSeq_wtc11.sh
+++ 2_call_variants/wtc11/2_using_deepvariant/variantCalling_deepVariant_isoSeq_wtc11.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 
+####### don't add read groups and don't mark/remove duplicates
+
+
 
 ### inputs
-REF=/home/vbarbo/project_2021/datasets/reference/GRCh38.p13_genome_only_chrm/GRCh38.p13_all_chr.fasta
-BAM_DIR=/home/vbarbo/project_2021/datasets/wtc11/manipulate_data
-DEEPVARIANT_DIR=/home/vbarbo/project_2021/datasets/wtc11/methods_to_comp/deepvariant
+REF=/home/vbarbo/project_2021/paper_analysis/reference/genome/GRCh38.p13_all_chr.fasta
+BAM_DIR=/home/vbarbo/project_2021/paper_analysis/wtc11/data_manipulation
+OUTPUT_DIR=/home/vbarbo/project_2021/paper_analysis/wtc11/variant_calling_from_isoseq/deepvariant
 THREADS=30
 
 
@@ -19,25 +22,25 @@ THREADS=30
 ### DV (alone)
 ###
 
-singularity exec --bind $DEEPVARIANT_DIR,/usr/lib/locale/ \
+singularity exec --bind $OUTPUT_DIR/dv/,/usr/lib/locale/ \
   /home/vbarbo/programs/deepvariant_singularity/deepvariant-1.1.0.simg \
   /opt/deepvariant/bin/run_deepvariant \
   --model_type PACBIO \
   --ref $REF \
   --reads $BAM_DIR/aln_s.bam \
-  --output_vcf $DEEPVARIANT_DIR/no_sncr/deepvariant_calls.vcf \
+  --output_vcf $OUTPUT_DIR/dv/deepvariant_calls.vcf \
   --num_shards $THREADS
 
 # filter only PASS
 bcftools view -f PASS \
-  $DEEPVARIANT_DIR/no_sncr/deepvariant_calls.vcf \
-  > $DEEPVARIANT_DIR/no_sncr/deepvariant_calls_pass.vcf
+  $OUTPUT_DIR/dv/deepvariant_calls.vcf \
+  > $OUTPUT_DIR/dv/deepvariant_calls_pass.vcf
 
 # compressing and indexing
-bgzip -c $DEEPVARIANT_DIR/no_sncr/deepvariant_calls_pass.vcf \
-  > $DEEPVARIANT_DIR/no_sncr/deepvariant_calls_pass.vcf.gz
-bcftools index $DEEPVARIANT_DIR/no_sncr/deepvariant_calls_pass.vcf.gz
-tabix -p vcf $DEEPVARIANT_DIR/no_sncr/deepvariant_calls_pass.vcf.gz
+bgzip -c $OUTPUT_DIR/dv/deepvariant_calls_pass.vcf \
+  > $OUTPUT_DIR/dv/deepvariant_calls_pass.vcf.gz
+bcftools index $OUTPUT_DIR/dv/deepvariant_calls_pass.vcf.gz
+tabix -p vcf $OUTPUT_DIR/dv/deepvariant_calls_pass.vcf.gz
 
 
 
@@ -46,25 +49,25 @@ tabix -p vcf $DEEPVARIANT_DIR/no_sncr/deepvariant_calls_pass.vcf.gz
 ### SNCR + DV
 ###
 
-singularity exec --bind $DEEPVARIANT_DIR,/usr/lib/locale/ \
+singularity exec --bind $OUTPUT_DIR/dv_sncr/,/usr/lib/locale/ \
   /home/vbarbo/programs/deepvariant_singularity/deepvariant-1.1.0.simg \
   /opt/deepvariant/bin/run_deepvariant \
   --model_type PACBIO \
   --ref $REF \
   --reads $BAM_DIR/aln_sncr.bam \
-  --output_vcf $DEEPVARIANT_DIR/yes_sncr_no_fc/deepvariant_calls.vcf \
+  --output_vcf $OUTPUT_DIR/dv_sncr/deepvariant_calls.vcf \
   --num_shards $THREADS
 
 # filter only PASS
 bcftools view -f PASS \
-  $DEEPVARIANT_DIR/yes_sncr_no_fc/deepvariant_calls.vcf \
-  > $DEEPVARIANT_DIR/yes_sncr_no_fc/deepvariant_calls_pass.vcf
+  $OUTPUT_DIR/dv_sncr/deepvariant_calls.vcf \
+  > $OUTPUT_DIR/dv_sncr/deepvariant_calls_pass.vcf
 
 # compressing and indexing
-bgzip -c $DEEPVARIANT_DIR/yes_sncr_no_fc/deepvariant_calls_pass.vcf \
-  > $DEEPVARIANT_DIR/yes_sncr_no_fc/deepvariant_calls_pass.vcf.gz
-bcftools index $DEEPVARIANT_DIR/yes_sncr_no_fc/deepvariant_calls_pass.vcf.gz
-tabix -p vcf $DEEPVARIANT_DIR/yes_sncr_no_fc/deepvariant_calls_pass.vcf.gz
+bgzip -c $OUTPUT_DIR/dv_sncr/deepvariant_calls_pass.vcf \
+  > $OUTPUT_DIR/dv_sncr/deepvariant_calls_pass.vcf.gz
+bcftools index $OUTPUT_DIR/dv_sncr/deepvariant_calls_pass.vcf.gz
+tabix -p vcf $OUTPUT_DIR/dv_sncr/deepvariant_calls_pass.vcf.gz
 
 
 
@@ -74,24 +77,24 @@ tabix -p vcf $DEEPVARIANT_DIR/yes_sncr_no_fc/deepvariant_calls_pass.vcf.gz
 ### SNCR + FC + DV
 ###
 
-singularity exec --bind $DEEPVARIANT_DIR,/usr/lib/locale/ \
+singularity exec --bind $OUTPUT_DIR/dv_sncr_fc/,/usr/lib/locale/ \
   /home/vbarbo/programs/deepvariant_singularity/deepvariant-1.1.0.simg \
   /opt/deepvariant/bin/run_deepvariant \
   --model_type PACBIO \
   --ref $REF \
   --reads $BAM_DIR/aln_sncr_fc.bam \
-  --output_vcf $DEEPVARIANT_DIR/deepvariant_calls.vcf \
+  --output_vcf $OUTPUT_DIR/dv_sncr_fc/deepvariant_calls.vcf \
   --num_shards $THREADS
 
 # filter only PASS
 bcftools view -f PASS \
-  $DEEPVARIANT_DIR/deepvariant_calls.vcf \
-  > $DEEPVARIANT_DIR/deepvariant_calls_pass.vcf
+  $OUTPUT_DIR/dv_sncr_fc/deepvariant_calls.vcf \
+  > $OUTPUT_DIR/dv_sncr_fc/deepvariant_calls_pass.vcf
 
 # compressing and indexing
-bgzip -c $DEEPVARIANT_DIR/deepvariant_calls_pass.vcf \
-  > $DEEPVARIANT_DIR/deepvariant_calls_pass.vcf.gz
-bcftools index $DEEPVARIANT_DIR/deepvariant_calls_pass.vcf.gz
-tabix -p vcf $DEEPVARIANT_DIR/deepvariant_calls_pass.vcf.gz
+bgzip -c $OUTPUT_DIR/dv_sncr_fc/deepvariant_calls_pass.vcf \
+  > $OUTPUT_DIR/dv_sncr_fc/deepvariant_calls_pass.vcf.gz
+bcftools index $OUTPUT_DIR/dv_sncr_fc/deepvariant_calls_pass.vcf.gz
+tabix -p vcf $OUTPUT_DIR/dv_sncr_fc/deepvariant_calls_pass.vcf.gz
 
 
