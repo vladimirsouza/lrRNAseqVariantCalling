8576531b2969cc3d9c9a97c04966d117866986fb
vladimirsouza <vladimirbcs@gmail.com>
2022-01-22 19:15:30 +0100

    Update WTC-11 variantCalling_clar3_isoSeq_wtc11.s

--- 2_call_variants/wtc11/4_using_clair3/variantCalling_clar3_isoSeq_wtc11.sh
+++ 2_call_variants/wtc11/4_using_clair3/variantCalling_clar3_isoSeq_wtc11.sh
@@ -28,7 +28,7 @@ conda install -c conda-forge -c bioconda whatshap=1.0 -y
 
 # clone Clair3
 git clone https://github.com/HKU-BAL/Clair3.git
-cd Clair3
+cd /home/vbarbo/programs/Clair3
 
 # download pre-trained models
 mkdir models
@@ -41,128 +41,120 @@ tar -zxvf clair3_models.tar.gz -C ./models
 ###### run clair3 on iso-seq ######
 ###################################
 
+
 conda deactivate
 conda activate clair3
 
 
-REF="/home/vbarbo/project_2021/datasets/reference/GRCh38.p13_genome_only_chrm/GRCh38.p13_all_chr.fasta"
-THREADS=30
+REF=/home/vbarbo/project_2021/paper_analysis/reference/genome/GRCh38.p13_all_chr.fasta
+INPUT_BAM_DIR=/home/vbarbo/project_2021/paper_analysis/wtc11/data_manipulation
+OUTPUT_DIR=/home/vbarbo/project_2021/paper_analysis/wtc11/variant_calling_from_isoseq/clair3
+THREADS=20
+
+
 
 
 ### Clair3 (alone)
-INPUT_BAM="/home/vbarbo/project_2021/datasets/wtc11/manipulate_data/aln_s.bam"
-OUTPUT_DIR="/home/vbarbo/project_2021/datasets/wtc11/methods_to_comp/clair3/alone"
 # call variants
 /home/vbarbo/programs/Clair3/run_clair3.sh \
-  --bam_fn=$INPUT_BAM \
+  --bam_fn=$INPUT_BAM_DIR/aln_s.bam \
   --ref_fn=$REF \
   --threads=$THREADS \
   --platform="hifi" \
   --model_path="/home/vbarbo/programs/Clair3/models/hifi" \
-  --output=$OUTPUT_DIR
+  --output=$OUTPUT_DIR/c3
 # filter only PASS
 bcftools view -f PASS \
-  $OUTPUT_DIR/pileup.vcf.gz \
-  > $OUTPUT_DIR/pileup_pass.vcf
+  $OUTPUT_DIR/c3/pileup.vcf.gz \
+  > $OUTPUT_DIR/c3/pileup_pass.vcf
 # compress and index
-bgzip -c $OUTPUT_DIR/pileup_pass.vcf \
-  > $OUTPUT_DIR/pileup_pass.vcf.gz
-bcftools index $OUTPUT_DIR/pileup_pass.vcf.gz
-tabix -p vcf $OUTPUT_DIR/pileup_pass.vcf.gz
-rm $OUTPUT_DIR/pileup_pass.vcf
+bgzip -c $OUTPUT_DIR/c3/pileup_pass.vcf \
+  > $OUTPUT_DIR/c3/pileup_pass.vcf.gz
+bcftools index $OUTPUT_DIR/c3/pileup_pass.vcf.gz
+tabix -p vcf $OUTPUT_DIR/c3/pileup_pass.vcf.gz
+rm $OUTPUT_DIR/c3/pileup_pass.vcf
 
 
 
 ### SNCR + Clair3
-INPUT_BAM="/home/vbarbo/project_2021/datasets/wtc11/manipulate_data/aln_sncr.bam"
-OUTPUT_DIR="/home/vbarbo/project_2021/datasets/wtc11/methods_to_comp/clair3/sncr"
 /home/vbarbo/programs/Clair3/run_clair3.sh \
-  --bam_fn=$INPUT_BAM \
+  --bam_fn=$INPUT_BAM_DIR/aln_sncr.bam \
   --ref_fn=$REF \
   --threads=$THREADS \
   --platform="hifi" \
   --model_path="/home/vbarbo/programs/Clair3/models/hifi" \
-  --output=$OUTPUT_DIR
+  --output=$OUTPUT_DIR/c3_sncr
 # filter only PASS
 bcftools view -f PASS \
-  $OUTPUT_DIR/pileup.vcf.gz \
-  > $OUTPUT_DIR/pileup_pass.vcf
+  $OUTPUT_DIR/c3_sncr/pileup.vcf.gz \
+  > $OUTPUT_DIR/c3_sncr/pileup_pass.vcf
 # compress and index
-bgzip -c $OUTPUT_DIR/pileup_pass.vcf \
-  > $OUTPUT_DIR/pileup_pass.vcf.gz
-bcftools index $OUTPUT_DIR/pileup_pass.vcf.gz
-tabix -p vcf $OUTPUT_DIR/pileup_pass.vcf.gz
-rm $OUTPUT_DIR/pileup_pass.vcf
+bgzip -c $OUTPUT_DIR/c3_sncr/pileup_pass.vcf \
+  > $OUTPUT_DIR/c3_sncr/pileup_pass.vcf.gz
+bcftools index $OUTPUT_DIR/c3_sncr/pileup_pass.vcf.gz
+tabix -p vcf $OUTPUT_DIR/c3_sncr/pileup_pass.vcf.gz
+rm $OUTPUT_DIR/c3_sncr/pileup_pass.vcf
+
 
 
 ### SNCR + FC + Clair3
-INPUT_BAM="/home/vbarbo/project_2021/datasets/wtc11/manipulate_data/aln_sncr_fc.bam"
-OUTPUT_DIR="/home/vbarbo/project_2021/datasets/wtc11/methods_to_comp/clair3/sncr_fc"
 /home/vbarbo/programs/Clair3/run_clair3.sh \
-  --bam_fn=$INPUT_BAM \
+  --bam_fn=$INPUT_BAM_DIR/aln_sncr_fc.bam \
   --ref_fn=$REF \
   --threads=$THREADS \
   --platform="hifi" \
   --model_path="/home/vbarbo/programs/Clair3/models/hifi" \
-  --output=$OUTPUT_DIR
+  --output=$OUTPUT_DIR/c3_sncr_fc
 # filter only PASS
 bcftools view -f PASS \
-  $OUTPUT_DIR/pileup.vcf.gz \
-  > $OUTPUT_DIR/pileup_pass.vcf
+  $OUTPUT_DIR/c3_sncr_fc/pileup.vcf.gz \
+  > $OUTPUT_DIR/c3_sncr_fc/pileup_pass.vcf
 # compress and index
-bgzip -c $OUTPUT_DIR/pileup_pass.vcf \
-  > $OUTPUT_DIR/pileup_pass.vcf.gz
-bcftools index $OUTPUT_DIR/pileup_pass.vcf.gz
-tabix -p vcf $OUTPUT_DIR/pileup_pass.vcf.gz
-rm $OUTPUT_DIR/pileup_pass.vcf
+bgzip -c $OUTPUT_DIR/c3_sncr_fc/pileup_pass.vcf \
+  > $OUTPUT_DIR/c3_sncr_fc/pileup_pass.vcf.gz
+bcftools index $OUTPUT_DIR/c3_sncr_fc/pileup_pass.vcf.gz
+tabix -p vcf $OUTPUT_DIR/c3_sncr_fc/pileup_pass.vcf.gz
+rm $OUTPUT_DIR/c3_sncr_fc/pileup_pass.vcf
 
 
 
-
-
-
-
+### Clair3-mix:
 ### SNPs from Clair3 (alone), indels from SNCR + FC+ Clair3
-OUTPUT_DIR="/home/vbarbo/project_2021/datasets/wtc11/methods_to_comp/clair3/mix"
-VCF_ALONE=/home/vbarbo/project_2021/datasets/wtc11/methods_to_comp/clair3/alone/pileup_pass.vcf.gz
-VCF_SNCR_FC=/home/vbarbo/project_2021/datasets/wtc11/methods_to_comp/clair3/sncr_fc/pileup_pass.vcf.gz
-
-
-mkdir $OUTPUT_DIR
 
 # snps
-vcftools --gzvcf $VCF_ALONE \
-  --out $OUTPUT_DIR/pileup_pass_snp \
+vcftools --gzvcf $OUTPUT_DIR/c3/pileup_pass.vcf.gz \
+  --out $OUTPUT_DIR/mix/pileup_pass_snp \
   --remove-indels --recode --recode-INFO-all
-bgzip $OUTPUT_DIR/pileup_pass_snp.recode.vcf
-tabix -p vcf $OUTPUT_DIR/pileup_pass_snp.recode.vcf.gz
+bgzip $OUTPUT_DIR/mix/pileup_pass_snp.recode.vcf
+tabix -p vcf $OUTPUT_DIR/mix/pileup_pass_snp.recode.vcf.gz
 
 # indels
-vcftools --gzvcf $VCF_SNCR_FC \
-  --out $OUTPUT_DIR/pileup_pass_indel \
+vcftools --gzvcf $OUTPUT_DIR/c3_sncr_fc/pileup_pass.vcf.gz \
+  --out $OUTPUT_DIR/mix/pileup_pass_indel \
   --keep-only-indels --recode --recode-INFO-all
-bgzip $OUTPUT_DIR/pileup_pass_indel.recode.vcf
-tabix -p vcf $OUTPUT_DIR/pileup_pass_indel.recode.vcf.gz
+bgzip $OUTPUT_DIR/mix/pileup_pass_indel.recode.vcf
+tabix -p vcf $OUTPUT_DIR/mix/pileup_pass_indel.recode.vcf.gz
 
 # concatenate vcf files
 bcftools concat \
-  $OUTPUT_DIR/pileup_pass_snp.recode.vcf.gz \
-  $OUTPUT_DIR/pileup_pass_indel.recode.vcf.gz \
-  -o $OUTPUT_DIR/pileup_pass_mix.recode.vcf.gz \
+  $OUTPUT_DIR/mix/pileup_pass_snp.recode.vcf.gz \
+  $OUTPUT_DIR/mix/pileup_pass_indel.recode.vcf.gz \
+  -o $OUTPUT_DIR/mix/pileup_pass_mix.recode.vcf.gz \
   -O z -D -a
 
-### to remove one of the (different) variants with same positions
-### >>> This is done in R
-library(vcfR)
-vcf <- read.vcfR("/home/vbarbo/project_2021/datasets/wtc11/methods_to_comp/clair3/mix/pileup_pass_mix.recode.vcf.gz")
-pos <- paste(vcf@fix[,1], vcf@fix[,2])
-k <- duplicated(pos)
-vcf_nodup <- vcf[!k]
-write.vcf(vcf_nodup, file="/home/vbarbo/project_2021/datasets/wtc11/methods_to_comp/clair3/mix/pileup_pass_mix_nodup.recode.vcf.gz")
-### <<< close R
-# for some reason, bcftools can't read vcf files produced by vcfR::write.vcf. 
-# decompressing and compressing it again can solve the problem!
-gunzip /home/vbarbo/project_2021/datasets/wtc11/methods_to_comp/clair3/mix/pileup_pass_mix_nodup.recode.vcf.gz
-bgzip /home/vbarbo/project_2021/datasets/wtc11/methods_to_comp/clair3/mix/pileup_pass_mix_nodup.recode.vcf
+# to remove one of the (different) variants with same positions
+Rscript -e "{
+  library(vcfR)
+  vcf <- read.vcfR('$OUTPUT_DIR/mix/pileup_pass_mix.recode.vcf.gz')
+  pos <- paste(vcf@fix[,1], vcf@fix[,2])
+  k <- duplicated(pos)
+  vcf_nodup <- vcf[!k]
+  write.vcf(vcf_nodup, file='$OUTPUT_DIR/mix/pileup_pass_mix_nodup.recode.vcf.gz')
+}"
+
+# for some reason, bcftools can't read vcf files produced by vcfR::write.vcf
+# decompressing and compressing it again can solve the problem!!
+gunzip $OUTPUT_DIR/mix/pileup_pass_mix_nodup.recode.vcf.gz
+bgzip $OUTPUT_DIR/mix/pileup_pass_mix_nodup.recode.vcf
 
 
