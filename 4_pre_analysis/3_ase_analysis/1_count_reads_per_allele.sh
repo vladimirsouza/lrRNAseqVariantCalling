e28e93d2d4ec27826e75148677b1cdba355c1e45
vladimirsouza <vladimirbcs@gmail.com>
2022-01-26 19:08:05 +0100

    Update 1_count_reads_per_allele.sh

--- 4_pre_analysis/3_ase_analysis/1_count_reads_per_allele.sh
+++ 4_pre_analysis/3_ase_analysis/1_count_reads_per_allele.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
 
 
+# Get the read count per allele for the ASE analysis.
+# This script creates a table with the read counts from short-read RNA-seq.
+
 
 
 #########################
 ###### align reads ######
 #########################
 
-REF_STAR_INDEX=/home/vbarbo/project_2021/datasets/reference/GRCh38.p13_genome_only_chrm/star_indexes/star_index_sjdbOverhang99
-REF_FASTA=/home/vbarbo/project_2021/datasets/reference/GRCh38.p13_genome_only_chrm/GRCh38.p13_all_chr.fasta
-GTF=/home/vbarbo/project_2021/datasets/reference/gtf_GRCh38.p13/gencode.v38.annotation.gtf
+REF_STAR_INDEX=/home/vbarbo/project_2021/paper_analysis/reference/genome/star_indexes/star_index_sjdbOverhang99
+REF_FASTA=/home/vbarbo/project_2021/paper_analysis/reference/genome/GRCh38.p13_all_chr.fasta
+GTF=/home/vbarbo/project_2021/paper_analysis/reference/gtf/gencode.v38.annotation.gtf
 SJDBOVERHANG=99
-THREADS=60
-S1R1=/home/vbarbo/project_2021/datasets/wtc11/data/rna_reads/illumina/GSE175048/SRR14637256.1_1.fastq.gz
-S1R2=/home/vbarbo/project_2021/datasets/wtc11/data/rna_reads/illumina/GSE175048/SRR14637256.1_2.fastq.gz
-S2R1=/home/vbarbo/project_2021/datasets/wtc11/data/rna_reads/illumina/GSE175048/SRR14637257.1_1.fastq.gz
-S2R2=/home/vbarbo/project_2021/datasets/wtc11/data/rna_reads/illumina/GSE175048/SRR14637257.1_2.fastq.gz
-S3R1=/home/vbarbo/project_2021/datasets/wtc11/data/rna_reads/illumina/GSE175048/SRR14637258.1_1.fastq.gz
-S3R2=/home/vbarbo/project_2021/datasets/wtc11/data/rna_reads/illumina/GSE175048/SRR14637258.1_2.fastq.gz
-S4R1=/home/vbarbo/project_2021/datasets/wtc11/data/rna_reads/illumina/ENCSR673UKZ/ENCLB122OCH_1.fastq.gz
-S4R2=/home/vbarbo/project_2021/datasets/wtc11/data/rna_reads/illumina/ENCSR673UKZ/ENCLB122OCH_2.fastq.gz
-S5R1=/home/vbarbo/project_2021/datasets/wtc11/data/rna_reads/illumina/ENCSR673UKZ/ENCLB366GPZ_1.fastq.gz
-S5R2=/home/vbarbo/project_2021/datasets/wtc11/data/rna_reads/illumina/ENCSR673UKZ/ENCLB366GPZ_2.fastq.gz
-S6R1=/home/vbarbo/project_2021/datasets/wtc11/data/rna_reads/illumina/ENCSR673UKZ/ENCLB979NPE_1.fastq.gz
-S6R2=/home/vbarbo/project_2021/datasets/wtc11/data/rna_reads/illumina/ENCSR673UKZ/ENCLB979NPE_2.fastq.gz
-OUT_PREFIX=/home/vbarbo/project_2021/datasets/wtc11/ase_analysis/GSE175048_ENCSR673UKZ/star_aln/GSE175048_ENCSR673UKZ_
+THREADS=30
+S1R1=/home/vbarbo/project_2021/paper_analysis/wtc11/data/rna_short_reads/GSE175048/SRR14637256.1_1.fastq.gz
+S1R2=/home/vbarbo/project_2021/paper_analysis/wtc11/data/rna_short_reads/GSE175048/SRR14637256.1_2.fastq.gz
+S2R1=/home/vbarbo/project_2021/paper_analysis/wtc11/data/rna_short_reads/GSE175048/SRR14637257.1_1.fastq.gz
+S2R2=/home/vbarbo/project_2021/paper_analysis/wtc11/data/rna_short_reads/GSE175048/SRR14637257.1_2.fastq.gz
+S3R1=/home/vbarbo/project_2021/paper_analysis/wtc11/data/rna_short_reads/GSE175048/SRR14637258.1_1.fastq.gz
+S3R2=/home/vbarbo/project_2021/paper_analysis/wtc11/data/rna_short_reads/GSE175048/SRR14637258.1_2.fastq.gz
+S4R1=/home/vbarbo/project_2021/paper_analysis/wtc11/data/rna_short_reads/ENCSR673UKZ/ENCLB122OCH_1.fastq.gz
+S4R2=/home/vbarbo/project_2021/paper_analysis/wtc11/data/rna_short_reads/ENCSR673UKZ/ENCLB122OCH_2.fastq.gz
+S5R1=/home/vbarbo/project_2021/paper_analysis/wtc11/data/rna_short_reads/ENCSR673UKZ/ENCLB366GPZ_1.fastq.gz
+S5R2=/home/vbarbo/project_2021/paper_analysis/wtc11/data/rna_short_reads/ENCSR673UKZ/ENCLB366GPZ_2.fastq.gz
+S6R1=/home/vbarbo/project_2021/paper_analysis/wtc11/data/rna_short_reads/ENCSR673UKZ/ENCLB979NPE_1.fastq.gz
+S6R2=/home/vbarbo/project_2021/paper_analysis/wtc11/data/rna_short_reads/ENCSR673UKZ/ENCLB979NPE_2.fastq.gz
+OUT_PREFIX=/home/vbarbo/project_2021/paper_analysis/wtc11/ase_analysis/star_aln/GSE175048_ENCSR673UKZ_
+
+SITES_VCF=/home/vbarbo/project_2021/paper_analysis/wtc11/ground_truth/3546dc62_AH77TTBBXX_DS-229105_GCCAAT_recalibrated_subsetChromosomes_pass.vcf.gz
+OUTPUT_TABLE_DIR=/home/vbarbo/project_2021/paper_analysis/wtc11/ase_analysis/ase_read_count_tables
+
+
+### to use 30 cores, intervals for the reference genome were already created in 
+### /home/vbarbo/project_2021/projects/lrRNAseqVariantCalling/1_generate_ground_truth/jurkat/generateGroundTruth_shortReads_gatk_jurkat.sh
+SCATTERED_INTERVAL_LIST=/home/vbarbo/project_2021/paper_analysis/reference/genome/interval_list/ref.scattered.interval_list
+THREADS=30
+loop_num=`expr $THREADS - 1`
+
 
 
 
@@ -75,13 +89,9 @@ samtools view \
 ### add read group ###
 ######################
 
-INPUT_BAM=/home/vbarbo/project_2021/datasets/wtc11/ase_analysis/GSE175048_ENCSR673UKZ/star_aln/GSE175048_ENCSR673UKZ_Aligned.sortedByCoord.out_primary.bam
-output_bam=${INPUT_BAM%.bam}_readGroupAddedSameOrder.bam
-
-#java -jar /home/vbarbo/programs/picard/build/libs/picard.jar AddOrReplaceReadGroups \
 java -XX:ParallelGCThreads=$THREADS -jar /home/vbarbo/programs/picard/build/libs/picard.jar AddOrReplaceReadGroups \
-  -I ${INPUT_BAM} \
-  -O ${output_bam} \
+  -I ${OUT_PREFIX}Aligned.sortedByCoord.out_primary.bam \
+  -O ${OUT_PREFIX}Aligned.sortedByCoord.out_primary_readGroupAddedSameOrder.bam \
   -VALIDATION_STRINGENCY LENIENT \
   -MAX_RECORDS_IN_RAM 5000000 \
   -ID 1 \
@@ -96,7 +106,7 @@ java -XX:ParallelGCThreads=$THREADS -jar /home/vbarbo/programs/picard/build/libs
 ### index bam
 samtools index \
   -@ ${THREADS} \
-  ${output_bam}
+  ${OUT_PREFIX}Aligned.sortedByCoord.out_primary_readGroupAddedSameOrder.bam
 
 
 
@@ -106,12 +116,6 @@ samtools index \
 # https://gatk.broadinstitute.org/hc/en-us/articles/360037054312-ASEReadCounter
 # compatible with Mamba, a downstream tool developed for allele-specific expression analysis
 # This tool will only process biallelic het SNP sites
-
-
-REF_FASTA=/home/vbarbo/project_2021/datasets/reference/GRCh38.p13_genome_only_chrm/GRCh38.p13_all_chr.fasta
-SITES_VCF=/home/vbarbo/project_2021/datasets/wtc11/truth/allen_institute_wtc11_shortreads_wgs/3546dc62_AH77TTBBXX_DS-229105_GCCAAT_recalibrated_subsetChromosomes_pass.vcf.gz
-INPUT_BAM=/home/vbarbo/project_2021/datasets/wtc11/ase_analysis/GSE175048_ENCSR673UKZ/star_aln/GSE175048_ENCSR673UKZ_Aligned.sortedByCoord.out_primary_readGroupAddedSameOrder.bam
-OUTPUT_TABLE_DIR=/home/vbarbo/project_2021/datasets/wtc11/ase_analysis/GSE175048_ENCSR673UKZ/ase_read_count_tables
 
 
 
@@ -127,26 +131,21 @@ vcftools --gzvcf ${SITES_VCF} \
 sites_vc_snp=${SITES_VCF%.vcf.gz}_snps.recode.vcf.gz
 bgzip -c \
   ${SITES_VCF%.vcf.gz}_snps.recode.vcf \
-  ${sites_vc_snp}
+  > ${sites_vc_snp}
 bcftools index ${sites_vc_snp}
 tabix -p vcf ${sites_vc_snp}
 rm ${SITES_VCF%.vcf.gz}_snps.recode.vcf
 
 
 
-
-
-SCATTERED_INTERVAL_LIST=/home/vbarbo/project_2021/datasets/gloria_data/analysis/my_ground_truth_jurkat_wgs_pe_100bp/jurkat100bp_scattered.interval_list
-THREADS=30
-loop_num=`expr $THREADS - 1`
-
+### read counts per allele
 mkdir ${OUTPUT_TABLE_DIR}
 
 for i in `seq -f '%04g' 0 $loop_num`
 do
   gatk --java-options "-Xmx4G -XX:+UseParallelGC -XX:ParallelGCThreads=1 -DGATK_STACKTRACE_ON_USER_EXCEPTION=true" ASEReadCounter \
     -R ${REF_FASTA} \
-    -I ${INPUT_BAM} \
+    -I ${OUT_PREFIX}Aligned.sortedByCoord.out_primary_readGroupAddedSameOrder.bam \
     -V ${sites_vc_snp} \
     -O ${OUTPUT_TABLE_DIR}/ase_read_count_$i.table \
     -L $SCATTERED_INTERVAL_LIST/$i-scattered.interval_list &
