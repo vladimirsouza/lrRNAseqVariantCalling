b342cfa0b1c87775432f6c28fdfed2d112fee78f
vladimirsouza <vladimirbcs@gmail.com>
2022-02-08 01:50:51 +0100

    Update script, lrRNAseqBenchmark has changed
    The splice_junction_analysis_table function no longer uses rds file paths
    as input. Now it uses the master table data.frame itself.
    
    Another change in the function is the new parameter `method_dataset_name`.

--- isoseq_variant_calling_analysis_code/5_pre_analysis/1_splice_junction_proximity_analysis/splice_junction_proximity.R
+++ isoseq_variant_calling_analysis_code/5_pre_analysis/1_splice_junction_proximity_analysis/splice_junction_proximity.R
@@ -7,19 +7,25 @@ library(lrRNAseqBenchmark)
 
 
 ### for snps
-master_table1 <- "/home/vbarbo/project_2021/paper_analysis/extra_files/master_tables/mt_jurkat_allMethods_filtered_v7.rds"
-master_table2 <- "/home/vbarbo/project_2021/paper_analysis/extra_files/master_tables/mt_wtc11_allMethods_filtered_v7.rds"
+rds_master_table1 <- "/home/vbarbo/project_2021/paper_analysis/extra_files/master_tables/mt_jurkat_allMethods_filtered_v7.rds"
+rds_master_table2 <- "/home/vbarbo/project_2021/paper_analysis/extra_files/master_tables/mt_wtc11_allMethods_filtered_v7.rds"
 min_isoseq_coverage <- 20
 method_names <- c("dv_s_fc", "c3_mix", "gatk_s")
 variant_type <- "snp"
 output_method_names <- c("SNCR+FC+DeepVariant", "Clair3-mix", "SNCR+GATK")
 truth_names <- c("merged", "allen")
 experiment_names <- c("Jurkat", "WTC-11")
+method_dataset_name <- c("isoSeq", "isoSeq")
+
+
+master_table1 <- readRDS(rds_master_table1)
+master_table2 <- readRDS(rds_master_table2)
 
 sj_proximity_snps <- splice_junction_analysis_table(master_table1,
                                                     master_table2,
                                                     experiment_names=experiment_names,
                                                     truth_names=truth_names,
+                                                    method_dataset_name=method_dataset_name,
                                                     method_names=method_names,
                                                     output_method_names=output_method_names,
                                                     variant_type=variant_type,
@@ -32,6 +38,7 @@ sj_proximity_indels <- splice_junction_analysis_table(master_table1,
                                                       master_table2,
                                                       experiment_names=experiment_names,
                                                       truth_names=truth_names,
+                                                      method_dataset_name=method_dataset_name,
                                                       method_names=method_names,
                                                       output_method_names=output_method_names,
                                                       variant_type=variant_type,
