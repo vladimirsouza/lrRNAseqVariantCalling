63489aa59689c9273b6e468d6a343100ba603d29
vladimirsouza <vladimirbcs@gmail.com>
2022-03-07 00:14:25 +0100

    Update script wtc11_iso_seq.sh
    file XGSUV_20200804_S64049_PL100158447-1_A01.ccs.bam was uploaded to
    NCBI SRA, and NEB_primers.fasta uploaded to zenodo.org/record/6332914.

--- isoseq_variant_calling_analysis_code/1_download_data/wtc11/iso_seq/wtc11_iso_seq.sh
+++ isoseq_variant_calling_analysis_code/1_download_data/wtc11/iso_seq/wtc11_iso_seq.sh
@@ -1,8 +1,18 @@
-# Download the Iso-Seq CCS reads for WTC-11
-wget https://sheynkman-lab-lifebit.s3.eu-west-1.amazonaws.com/Sample-Data/WTC11/XGSUV_20200804_S64049_PL100158447-1_A01.ccs.bam \
-  -P /home/vbarbo/project_2021/paper_analysis/wtc11/data/iso_seq/
+# Iso-Seq reads (PacBio BAM) is downloaded from https://www.ncbi.nlm.nih.gov/sra/SRX14278671[accn]
 
-# To remove primers, we used the following FASTA files of primer sequences.
-wget https://sheynkman-lab-lifebit.s3.eu-west-1.amazonaws.com/Reference/NEB_primers.fasta \
-  -P /home/vbarbo/project_2021/paper_analysis/wtc11/data/iso_seq/
+cd /home/vbarbo/project_2021/paper_analysis/wtc11/data/iso_seq
 
+wget https://sra-download.ncbi.nlm.nih.gov/traces/sra33/SRR/017705/SRR18130587
+
+sam-dump --unaligned SRR18130587 > XGSUV_20200804_S64049_PL100158447-1_A01.ccs.sam
+
+samtools view -S -b XGSUV_20200804_S64049_PL100158447-1_A01.ccs.sam > XGSUV_20200804_S64049_PL100158447-1_A01.ccs.bam
+
+rm SRR18130587 \
+  XGSUV_20200804_S64049_PL100158447-1_A01.ccs.sam
+
+
+
+# To remove primers with limma, download primer fasta
+
+wget https://zenodo.org/record/6332914/files/NEB_primers.fasta
